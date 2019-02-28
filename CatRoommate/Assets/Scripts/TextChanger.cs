7708168ff9c1eb20b16b8e453364a9113836294a
Raveena <rp2971@nyu.edu>
2019-02-28 12:38:49 -0500

    Added the example back and cat.png file

---
+++ CatRoommate/Assets/Scripts/TextChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextChanger : MonoBehaviour
+{
+    public TextMeshProUGUI crunchy;
+    public string[] crunchyStuff;
+
+    public void ChangeText()
+    {
+        int randNum;
+        randNum = Random.Range(0, crunchyStuff.Length);
+        crunchy.text = crunchyStuff[randNum];
+    }
+}
