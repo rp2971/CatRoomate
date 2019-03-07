bb97f4a4d053dd1332028b4279e35d2fa2e45545
Raveena <rp2971@nyu.edu>
2019-03-07 12:43:34 -0500

    String stuff

---
+++ CatRoommate/Assets/Scripts/ConversationManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using TMPro;
+
+
+//[System.Serializable]
+public class button : MonoBehaviour
+{
+    static void Main ()
+    {
+        string[] dialogue = new string[7];
+        dialogue[0] = "I have a fully furnished 2 bedroom & I need a roommate! Just a few questions tho…";
+        dialogue[1] = "Are you a night owl or an early riser?";
+        dialogue[2] = "What do you like on your pizza?";
+        dialogue[3] = "Are you allergic to cats?";
+        dialogue[4] = "How do you feel about naps?";
+        dialogue[5] = "Don’t you hate birds?";
+        dialogue[6] = "Can I have some belly rubs?";
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
