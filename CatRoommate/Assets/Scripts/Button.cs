458fb225b670b08346902f078da6273938508e8b
Raveena <rp2971@nyu.edu>
2019-03-12 20:51:12 -0400

    Added scenes and modified scripts

--- CatRoommate/Assets/Scripts/Button.cs
+++ CatRoommate/Assets/Scripts/Button.cs
@@ -6,12 +6,16 @@ using TMPro;
 
 
 //[System.Serializable]
-public class button : MonoBehaviour
+public class Button : MonoBehaviour
 {
+
+
+
+
     static void Main ()
     {
         string[] dialogue = new string[7];
-        dialogue[0] = "I have a fully furnished 2 bedroom & I need a roommate! Just a few questions tho…";
+        dialogue[0] = "I have a fully furnished place with two bedrooms & I need a roommate! Just a few questions tho…";
         dialogue[1] = "Are you a night owl or an early riser?";
         dialogue[2] = "What do you like on your pizza?";
         dialogue[3] = "Are you allergic to cats?";
--- CatRoommate/Assets/Scripts/GameController.cs
+++ CatRoommate/Assets/Scripts/GameController.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        DontDestroyOnLoad(this.gameObject);
     }
 
     // Update is called once per frame
